9096e67ba904e851713d1fb38ebabfbc6aef692d
maksym blashko <mblashko@axon.dev>
2023-11-09 13:10:50 +0200

    Init commit

---
+++ AxonML/AxonML.h
@@ -0,0 +1,18 @@
+//
+//  AxonML.h
+//  AxonML
+//
+//  Created by Blashko Maksym on 09.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AxonML.
+FOUNDATION_EXPORT double AxonMLVersionNumber;
+
+//! Project version string for AxonML.
+FOUNDATION_EXPORT const unsigned char AxonMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AxonML/PublicHeader.h>
+
+
